5815e89b5b755ca3e9dfbb92ef555e0e1418454e
jeff oriente <jdanieloriente@gmail.com>
2019-06-14 04:44:11 -0400

    integrated firebase and beginning to make chat rooms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+import RoomList from './Components/RoomList';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA34rEwyjTrRNW510TEsrxQQb8UCFuVGL4",
+    authDomain: "bloc-chat-react-4ad6d.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-4ad6d.firebaseio.com",
+    projectId: "bloc-chat-react-4ad6d",
+    storageBucket: "bloc-chat-react-4ad6d.appspot.com",
+    messagingSenderId: "250871938568",
+    appId: "1:250871938568:web:228ec14176f4ce68"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RoomList firebase={firebase}/>
     </div>
   );
 }
