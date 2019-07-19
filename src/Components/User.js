c7a9afc56313722703584cf2388562bcc99726dd
jeff oriente <jdanieloriente@gmail.com>
2019-07-19 19:06:26 -0400

    trying to make .then method display username in signUp method

--- src/Components/User.js
+++ src/Components/User.js
@@ -10,7 +10,17 @@ class User extends Component {
 
   signIn() {
     const provider = new this.props.firebase.auth.GoogleAuthProvider();
-    this.props.firebase.auth().signInWithPopup( provider );
+    this.props.firebase.auth().signInWithPopup( provider ).then(function(result) {
+      const token = result.credential.accessToken;
+      const user = result.user;
+      this.props.setUser(user);
+      {console.log(this.props.user)}
+    }).catch(function(error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email
+      const credential = error.credential;
+    });
   }
 
   signOut() {
@@ -21,7 +31,6 @@ class User extends Component {
     return (
       <section>
         <div>{this.props.user ? this.props.user.displayName : 'Guest'}</div>
-        
           <button onClick={() => this.signIn()}>Sign in</button>
           <button onClick={() => this.signOut()}>Sign out</button>
 
